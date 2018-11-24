59420396ced0c2990cc8c7f7b0d5266714fc30be
Alex Shelley <ashelley@purdue.edu>
2018-11-24 10:39:23 -0500

    wrote tests for getting tags on folders.

--- TaggingTests/TaggingTests.cs
+++ TaggingTests/TaggingTests.cs
@@ -38,5 +38,12 @@ namespace TaggingTests
         [TestMethod] public void JustFooFile()  => AssertFileTags("file[foo].txt", "foo");
         [TestMethod] public void JustBarFile()  => AssertFileTags("file[bar].txt", "bar");
         [TestMethod] public void FooBarFile()   => AssertFileTags("file[foo bar].txt", "foo", "bar");
+
+        [TestMethod] public void UntaggedFolder() => AssertFolderTags("no_tags");
+        [TestMethod] public void JustFooFolder() => AssertFolderTags("just_foo", "foo");
+        [TestMethod] public void JustBarFolder() => AssertFolderTags("just_bar", "bar");
+        [TestMethod] public void FooBarFolder() => AssertFolderTags("foo_bar", "foo", "bar");
+        [TestMethod] public void BarFooFolder() => AssertFolderTags("bar_foo", "foo", "bar");
+
     }
 }
