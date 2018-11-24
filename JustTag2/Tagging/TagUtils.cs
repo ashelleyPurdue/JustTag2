70bb7512046146ad7b9a501e20eed3c82ddc22b0
Alex Shelley <ashelley@purdue.edu>
2018-11-23 19:35:39 -0500

    Stubbed out the methods GetTags and SetTags.  Updated to TabbedFileBrowser 0.0.0.3, which adds search support.

---
+++ JustTag2/Tagging/TagUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JustTag2.Tagging
+{
+    public static class TagUtils
+    {
+        /// <summary>
+        /// Returns the tags on the given file or folder
+        /// </summary>
+        public static string[] GetTags(FileSystemInfo file) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Sets the tags on the given file or folder.
+        /// Returns a new FileSystemInfo with the updated tags.
+        /// </summary>
+        public static FileSystemInfo SetTags(FileSystemInfo file, string[] tags) => throw new NotImplementedException();
+    }
+}
