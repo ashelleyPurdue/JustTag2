efb6655b34b026e61a747ec948d928de6f000ce3
Alex Shelley <ashelley@purdue.edu>
2018-11-23 20:36:56 -0500

    Created unit test project.

---
+++ TaggingTests/TaggingTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using JustTag2.Tagging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaggingTests
+{
+    [TestClass]
+    public class TaggingTests
+    {
+    }
+}
