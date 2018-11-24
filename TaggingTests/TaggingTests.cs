35046862e44760774e0c71c80f8793caae4317e2
Alex Shelley <ashelley@purdue.edu>
2018-11-23 20:48:06 -0500

    Wrote tests for GetTags on files.

--- TaggingTests/TaggingTests.cs
+++ TaggingTests/TaggingTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
 using JustTag2.Tagging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,5 +9,20 @@ namespace TaggingTests
     [TestClass]
     public class TaggingTests
     {
+        public static void AssertFileTags(string fileName, params string[] expectedTags)
+        {
+            var file = new FileInfo(fileName);
+            var actualTags = TagUtils.GetTags(file);
+
+            var expectedSorted = expectedTags.OrderBy(f => f);
+            var actualSorted = actualTags.OrderBy(f => f);
+
+            Assert.IsTrue(actualSorted.SequenceEqual(expectedSorted));
+        }
+
+        [TestMethod] public void Untagged() => AssertFileTags("no_tags.txt");
+        [TestMethod] public void JustFoo()  => AssertFileTags("file[foo].txt", "foo");
+        [TestMethod] public void JustBar()  => AssertFileTags("file[bar].txt", "bar");
+        [TestMethod] public void FooBar()   => AssertFileTags("file[foo bar].txt", "foo", "bar");
     }
 }
