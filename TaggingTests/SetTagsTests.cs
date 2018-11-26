1b5e43b49e5b192e9dcb7f4b4d1de202ef949a35
Alex Shelley <ashelley@purdue.edu>
2018-11-26 18:23:36 -0500

    Renamed the unit test files.

---
+++ TaggingTests/SetTagsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaggingTests
+{
+    [TestClass]
+    public class SetTagsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
