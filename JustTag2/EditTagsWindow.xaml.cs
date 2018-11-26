7584cff62cc1e1a456890b00434387d9ebdb5965
Alex Shelley <ashelley@purdue.edu>
2018-11-25 22:34:49 -0500

    Created a stub EditTags window.

--- JustTag2/EditTagsWindow.xaml.cs
+++ JustTag2/EditTagsWindow.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TabbedFileBrowser;
 
 namespace JustTag2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for EditTagsWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EditTagsWindow : Window
     {
-        public MainWindow()
+        public EditTagsWindow()
         {
             InitializeComponent();
-            browser.ViewModel.ParseFilterString = Tagging.TagUtils.ParseFilterString;
-        }
-
-        private void EditTagsMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            // TODO: Tell it which file to edit
-            new EditTagsWindow().ShowDialog();
         }
     }
 }
