03d9bcc929b94e19ebdc399da981ded32f45ccd8
Alex Shelley <ashelley@purdue.edu>
2018-11-28 22:06:51 -0500

    implemented GetPreviewer.

--- JustTag2/Previewers/MainPreviewer.xaml.cs
+++ JustTag2/Previewers/MainPreviewer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace JustTag2.Previewers
 {
@@ -22,12 +23,30 @@ namespace JustTag2.Previewers
     {
         private IPreviewer[] previewers = new IPreviewer[]
         {
-
+            new ImagePreviewer(),
+            new FallbackPreviewer()
         };
+        private IPreviewer currentPreviewer;
 
         public MainPreviewer()
         {
             InitializeComponent();
+
+            // Add all of the previewers' controls, but make them hidden.
+            foreach (var p in previewers)
+            {
+                p.Control.Visibility = Visibility.Collapsed;
+                AddChild(p.Control);
+            }
         }
+
+        /// <summary>
+        /// Returns the previewer used to display the given file.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private IPreviewer GetPreviewer(FileSystemInfo file) => previewers
+            .Where(p => p.CanPreview(file))
+            .First();
     }
 }
