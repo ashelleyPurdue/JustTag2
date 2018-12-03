8422b83fbcb3db053db171918030621d42e00f9c
Alex Shelley <ashelley@purdue.edu>
2018-12-02 19:22:28 -0500

    Started VideoPreviewer, wrote CanPreview

---
+++ JustTag2/Previewers/VideoPreviewer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JustTag2.Previewers
+{
+    /// <summary>
+    /// Interaction logic for VideoPreviewer.xaml
+    /// </summary>
+    public partial class VideoPreviewer : UserControl, IPreviewer
+    {
+        public VideoPreviewer()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl Control => this;
+
+        public bool CanPreview(FileSystemInfo file)
+        {
+            // TODO: Find a way to reduce the duplicate code.
+            string[] extensions = new[]
+            {
+                ".gif",
+                ".gifv",
+                ".webm",
+                ".mpg",
+                ".mpeg",
+                ".wmv",
+                ".mp4",
+                ".mov"  // TODO: Other formats that I can't remember off the top of my head
+            };
+
+            return extensions.Contains(file.Extension.ToLower());
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open(FileSystemInfo file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
