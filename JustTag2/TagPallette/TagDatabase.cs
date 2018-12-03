afff4f57c2940b54af28f8e35211d8ce99c6cde5
Alex Shelley <ashelley@purdue.edu>
2018-12-03 18:43:21 -0500

    Created stub for TagDatabase class.

---
+++ JustTag2/TagPallette/TagDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustTag2.TagPallette
+{
+    public class TagDatabase
+    {
+        public List<TagCategory> categories;
+
+        public TagDatabase(string filePath) => throw new NotImplementedException();
+
+        public void Save(string filePath) => throw new NotImplementedException();
+        public void Load(string filePath) => throw new NotImplementedException();
+    }
+
+    public class TagCategory
+    {
+        public string name;
+        public string desc;
+
+        public List<Tag> tags;
+    }
+
+    public class Tag
+    {
+        public string name;
+        public string desc;
+    }
+}
