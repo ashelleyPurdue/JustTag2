d57953732e236b72f41d6328accf49fb5ecb3734
Alex Shelley <ashelley@purdue.edu>
2018-12-21 15:11:03 -0500

    Began writing TagTextbox

---
+++ JustTag2/TagTextbox.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JustTag2
+{
+    /// <summary>
+    /// Interaction logic for TagTextbox.xaml
+    /// </summary>
+    public partial class TagTextbox : UserControl
+    {
+        public TagTextbox()
+        {
+            InitializeComponent();
+        }
+
+        private void TypingBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If the user types whitespace, add the tag they were typing to the list
+            var whitespaceKeys = new Key[]
+            {
+                Key.Space,
+                Key.Tab,
+                Key.Enter
+            };
+
+            if (whitespaceKeys.Contains(e.Key))
+            {
+                e.Handled = true;
+
+                string tag = typingBox.Text;
+                tagsList.Items.Add(tag);
+                typingBox.Clear();
+            }
+        }
+    }
+}
