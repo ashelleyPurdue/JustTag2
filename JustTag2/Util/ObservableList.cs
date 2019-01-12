a547b1e73b4bd42c8870c567dede162e438937c3
Alex Shelley <ashelley@purdue.edu>
2019-01-12 15:08:13 -0500

    ObservableList now raises CollectionChanged when one of its items sends a PropertyChanged event.

--- JustTag2/Util/ObservableList.cs
+++ JustTag2/Util/ObservableList.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace JustTag2.Util
 {
-    public class ObservableList<T> : ObservableCollection<T>, IList<T>
+    /// <summary>
+    /// Like ObservableCollection, except it also notifies if one of its elements
+    /// has a property changed event
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ObservableList<T> : ObservableCollection<T>
     {
+        public ObservableList()
+        {
+            base.CollectionChanged += ObservableList_CollectionChanged;
+        }
+
+        private void ObservableList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            foreach (T item in e.OldItems)
+            {
+                if (item is INotifyPropertyChanged i)
+                    i.PropertyChanged -= Item_PropertyChanged;
+            }
+
+            foreach (T item in e.NewItems)
+            {
+                if (item is INotifyPropertyChanged i)
+                    i.PropertyChanged += Item_PropertyChanged;
+            }
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var args = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, sender);
+            this.OnCollectionChanged(args);
+        }
     }
 }
