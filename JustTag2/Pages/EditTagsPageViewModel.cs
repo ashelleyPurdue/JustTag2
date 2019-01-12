46c0e9e85a03b174e16e3eba8513d7acba26445f
Alex Shelley <ashelley@purdue.edu>
2019-01-12 14:33:46 -0500

    Began working on EditTagsPageViewModel.

---
+++ JustTag2/Pages/EditTagsPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using JustTag2.Util;
+
+namespace JustTag2.Pages
+{
+    public interface IEditTagsPageViewModel
+    {
+        FileSystemInfo CurrentFile { get; }
+        IList<string> Tags { get; }
+    }
+
+    public class EditTagsPageViewModel : IEditTagsPageViewModel, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public FileSystemInfo CurrentFile { get; set; }
+        public ObservableList<string> Tags { get; set; }
+        IList<string> IEditTagsPageViewModel.Tags => this.Tags;
+
+        public void Save()
+        {
+            
+        }
+
+    }
+}
---
+++ JustTag2/Util/ObservableList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustTag2.Util
+{
+    public class ObservableList<T> : ObservableCollection<T>, IList<T>
+    {
+    }
+}
