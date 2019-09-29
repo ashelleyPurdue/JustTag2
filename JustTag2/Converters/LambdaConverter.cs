6a164b17603b9969188193f9e61fddc671810b9e
Alex Shelley <alexthepeppershelley@gmail.com>
2019-09-29 15:20:42 -0400

    the edit tags button now uses a converter instead of a style

---
+++ JustTag2/Converters/LambdaConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JustTag2.Converters
+{
+    public abstract class LambdaConverter<TIn, TOut> : IValueConverter
+    {
+        public abstract Func<TIn, TOut> ConvertFunc { get; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => ConvertFunc((TIn)value);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ JustTag2/Converters/VisibleIfNotNull.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JustTag2.Converters
+{
+    public class VisibleIfNotNull : LambdaConverter<object, Visibility>
+    {
+        public override Func<object, Visibility> ConvertFunc => obj => (obj == null)
+            ? Visibility.Hidden
+            : Visibility.Visible;
+    }
+}
