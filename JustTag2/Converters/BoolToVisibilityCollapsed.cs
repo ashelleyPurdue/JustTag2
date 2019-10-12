6c76e5c64dafebd59501c1ecf3c48d88991d1809
Alex Shelley <alexthepeppershelley@gmail.com>
2019-10-12 17:16:30 -0400

    Add converter BoolToVisibilityCollapsed

---
+++ JustTag2/Converters/BoolToVisibilityCollapsed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace JustTag2.Views
+{
+    public class BoolToVisibilityCollapsed : LambdaConverter<bool, Visibility>
+    {
+        public override Func<bool, Visibility> ConvertFunc => value => value
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+}
