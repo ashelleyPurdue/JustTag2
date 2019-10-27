75a9893158252c8e767f47028638792b07d9dfab
Alex Shelley <alexthepeppershelley@gmail.com>
2019-10-27 16:37:42 -0400

    Add OnPropertyChanged and UnregisterPropertyChanged to ViewModelBase

--- JustTag2/ViewModels/ViewModelBase.cs
+++ JustTag2/ViewModels/ViewModelBase.cs
@@ -8,6 +8,47 @@ namespace JustTag2
     public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private Dictionary<string, HashSet<Action>> propertyChangedHandlers = new Dictionary<string, HashSet<Action>>();
+
+        public ViewModelBase()
+        {
+            // Invoke all registered property changed handlers.
+            PropertyChanged += (s, e) =>
+            {
+                var handlers = GetOrCreateHandlers(e.PropertyName);
+
+                foreach (var handler in handlers)
+                    handler();
+            };
+        }
+
+        /// <summary>
+        /// Invokes the given handler when the property with the given name changes.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="handler"></param>
+        public void OnPropertyChanged(string propertyName, Action handler)
+        {
+            GetOrCreateHandlers(propertyName).Add(handler);
+        }
+
+        /// <summary>
+        /// Unsubscribes the given handler from property changed notifications.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="handler"></param>
+        public void UnregisterPropertyChanged(string propertyName, Action handler)
+        {
+            GetOrCreateHandlers(propertyName).Remove(handler);
+        }
+
+        private HashSet<Action> GetOrCreateHandlers(string propertyName)
+        {
+            if (!propertyChangedHandlers.ContainsKey(propertyName))
+                propertyChangedHandlers.Add(propertyName, new HashSet<Action>());
+
+            return propertyChangedHandlers[propertyName];
+        }
 
         protected void SetAndRaise<T>(ref T backingField, T value, params string[] properties)
         {
